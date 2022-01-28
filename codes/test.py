9a509a8f47ad3d637e653f0b1d2b208168f46ed4
Seung Ho Park <80231716+seungho-snu@users.noreply.github.com>
2022-01-28 22:39:02 +0900

    Update test.py

--- codes/test.py
+++ codes/test.py
@@ -2,17 +2,14 @@ import os.path as osp
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import logging
-import time
 import argparse
-from collections import OrderedDict
 
 import options.options as option
 import utils.util as util
-from data.util import bgr2ycbcr
 from data import create_dataset, create_dataloader
-from models import create_model
+from models.SR_model import SRModel
 
-def esrgan():
+def fxsr():
     #### options
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--T_ctrl', type=float, default=0.8)
@@ -24,12 +21,9 @@ def esrgan():
     opt = option.dict_to_nonedict(opt)
     opt.T_ctrl = T_ctrl_temp
     T_ctrl_str = '%03d' % (opt.T_ctrl * 100)
-    # opt['name'] = opt['name'] + '_w' + sss
     opt['name'] = opt['name'] + '_t' + T_ctrl_str
     opt['path']['results_root'] = opt['path']['results_root'] + '_t' + T_ctrl_str
     opt['path']['log'] = opt['path']['log'] + '_t' + T_ctrl_str
-
-    # opt = parser.parse_args()
 
     util.mkdirs(
         (path for key, path in opt['path'].items()
@@ -43,23 +37,17 @@ def esrgan():
     test_loaders = []
     for phase, dataset_opt in sorted(opt['datasets'].items()):
         test_set = create_dataset(dataset_opt)
-        test_loader = create_dataloader(test_set, dataset_opt)
+        test_loader = create_dataloader(test_set)
         logger.info('Number of test images in [{:s}]: {:d}'.format(dataset_opt['name'], len(test_set)))
         test_loaders.append(test_loader)
 
-    model = create_model(opt)
+    model = SRModel(opt)
+
     for test_loader in test_loaders:
         test_set_name = test_loader.dataset.opt['name']
         logger.info('\nTesting [{:s}]...'.format(test_set_name))
-        test_start_time = time.time()
         dataset_dir = osp.join(opt['path']['results_root'], test_set_name)
         util.mkdir(dataset_dir)
-
-        test_results = OrderedDict()
-        test_results['psnr'] = []
-        test_results['ssim'] = []
-        test_results['psnr_y'] = []
-        test_results['ssim_y'] = []
 
         for data in test_loader:
             need_GT = False if test_loader.dataset.opt['dataroot_GT'] is None else True
@@ -79,67 +67,8 @@ def esrgan():
             else:
                 save_img_path = osp.join(dataset_dir, img_name + '.png')
             util.save_img(sr_img, save_img_path)
-            #
-            # # img = scipy.misc.imread(sys.argv[1], flatten=True).astype(numpy.float)/255.0
-            # # print ("NIQE = %f" %(NQ.niqe(sr_img)))
-            #
-            # # calculate PSNR and SSIM
-            # if need_GT:
-            #     gt_img = util.tensor2img(visuals['GT'])
-            #     gt_img = gt_img / 255.
-            #     sr_img = sr_img / 255.
-            #
-            #     crop_border = opt['crop_border'] if opt['crop_border'] else opt['scale']
-            #     if crop_border == 0:
-            #         cropped_sr_img = sr_img
-            #         cropped_gt_img = gt_img
-            #     else:
-            #         cropped_sr_img = sr_img[crop_border:-crop_border, crop_border:-crop_border, :]
-            #         cropped_gt_img = gt_img[crop_border:-crop_border, crop_border:-crop_border, :]
-            #
-            #     # psnr = util.calculate_psnr(cropped_sr_img * 255, cropped_gt_img * 255)
-            #     # ssim = util.calculate_ssim(cropped_sr_img * 255, cropped_gt_img * 255)
-            #     psnr = 0.0
-            #     ssim = 0.0
-            #     test_results['psnr'].append(psnr)
-            #     test_results['ssim'].append(ssim)
-            #
-            #     if gt_img.shape[2] == 3:  # RGB image
-            #         sr_img_y = bgr2ycbcr(sr_img, only_y=True)
-            #         gt_img_y = bgr2ycbcr(gt_img, only_y=True)
-            #         if crop_border == 0:
-            #             cropped_sr_img_y = sr_img_y
-            #             cropped_gt_img_y = gt_img_y
-            #         else:
-            #             cropped_sr_img_y = sr_img_y[crop_border:-crop_border, crop_border:-crop_border]
-            #             cropped_gt_img_y = gt_img_y[crop_border:-crop_border, crop_border:-crop_border]
-            #         # psnr_y = util.calculate_psnr(cropped_sr_img_y * 255, cropped_gt_img_y * 255)
-            #         # ssim_y = util.calculate_ssim(cropped_sr_img_y * 255, cropped_gt_img_y * 255)
-            #         psnr_y = 0.0
-            #         ssim_y = 0.0
-            #         test_results['psnr_y'].append(psnr_y)
-            #         test_results['ssim_y'].append(ssim_y)
-            #         # logger.info(
-            #         #     '{:20s} - PSNR: {:.6f} dB; SSIM: {:.6f}; PSNR_Y: {:.6f} dB; SSIM_Y: {:.6f}.'.
-            #         #     format(img_name, psnr, ssim, psnr_y, ssim_y))
-            #     # else:
-            #     #     logger.info('{:20s} - PSNR: {:.6f} dB; SSIM: {:.6f}.'.format(img_name, psnr, ssim))
-            # else:
+
             logger.info(img_name)
-        #
-        # if need_GT:  # metrics
-        #     # Average PSNR/SSIM results
-        #     ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
-        #     ave_ssim = sum(test_results['ssim']) / len(test_results['ssim'])
-        #     logger.info(
-        #         '----Average PSNR/SSIM results for {}----\n\tPSNR: {:.6f} dB; SSIM: {:.6f}\n'.format(
-        #             test_set_name, ave_psnr, ave_ssim))
-        #     if test_results['psnr_y'] and test_results['ssim_y']:
-        #         ave_psnr_y = sum(test_results['psnr_y']) / len(test_results['psnr_y'])
-        #         ave_ssim_y = sum(test_results['ssim_y']) / len(test_results['ssim_y'])
-        #         logger.info(
-        #             '----Y channel, average PSNR/SSIM----\n\tPSNR_Y: {:.6f} dB; SSIM_Y: {:.6f}\n'.
-        #             format(ave_psnr_y, ave_ssim_y))
 
 if __name__ == '__main__':
-    esrgan()
+    fxsr()
